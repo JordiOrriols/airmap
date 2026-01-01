5a7c8678f48f8b4b2434196875a3079665da26aa
Jordi Orriols <info@jordiorriols.cat>
2026-01-02 00:20:56 +0100

    chore: fix typescript build

--- src/lib/react-query.ts
+++ src/lib/react-query.ts
@@ -1,4 +1,4 @@
-import { QueryClient, UseQueryOptions } from "@tanstack/react-query";
+import { QueryClient } from "@tanstack/react-query";
 
 /**
  * Default query options for all React Query hooks
